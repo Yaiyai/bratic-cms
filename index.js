525c8d3cbb55635d5e08b4968808bc3d94cd8930
Yai <myd.rio@hotmail.com>
2020-12-20 18:54:16 +0100

    login and signup working

--- index.js
+++ index.js
@@ -19,9 +19,12 @@ app.use(express.static('public'))
 //Lectura y parseo del body
 app.use(express.json())
 
+//Rutas
+app.use('/api/auth', require('./routes/auth.routes'))
+
 //Escuchar peticiones
 app.listen(process.env.PORT, () => console.log(`Servidor establecido en puerto ${process.env.PORT}`))
 
-app.use((req, res) => {
-	res.sendFile(__dirname + '/public/index.html')
-})
+// app.use((req, res) => {
+// 	res.sendFile(__dirname + '/public/index.html')
+// })
