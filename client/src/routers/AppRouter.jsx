c8c762d449da072d9f8ab7c221a7d5cea016180e
Yai <myd.rio@hotmail.com>
2021-01-06 13:55:26 +0100

    working on front

--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import LoginScreen from '../components/auth/LoginScreen'
 import SignupScreen from '../components/auth/SignupScreen'
+import { DashboardScreen } from '../components/DashboardScreen'
+import { AuthContext } from '../reducers/auth/AuthContext'
+import { DashboardNav } from '../ui/DashboardNav'
 import { Navbar } from '../ui/Navbar'
 
 const AppRouter = () => {
+	const { user } = useContext(AuthContext)
+
 	return (
 		<Router>
 			<div>
-				<Navbar />
-				<main className='container'>
-					<Switch>
-						<Route exact path='/' component={LoginScreen} />
-						<Route path='/registro' component={SignupScreen} />
-						<Redirect to='/' />
-					</Switch>
-					<small>Made with &hearts; by Yai</small>
-					<small>&copy; Bratic S.L.</small>
-				</main>
+				{user.token ? (
+					<div className='dashboard-container'>
+						<DashboardNav />
+
+						<main>
+							<Switch>
+								<Route exact path='/bratic' component={DashboardScreen} />
+								<Redirect to='/bratic' />
+							</Switch>
+						</main>
+					</div>
+				) : (
+					<>
+						<Navbar />
+						<main className='container'>
+							<Switch>
+								<Route exact path='/' component={LoginScreen} />
+								<Route path='/registro' component={SignupScreen} />
+								<Redirect to='/' />
+							</Switch>
+							<small>Made with &hearts; by Yai</small>
+							<small>&copy; Bratic S.L.</small>
+						</main>
+					</>
+				)}
 			</div>
 		</Router>
 	)
