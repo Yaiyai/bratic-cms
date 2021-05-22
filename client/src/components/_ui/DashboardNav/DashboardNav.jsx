771d61ba034d9214981185d2bfa5a3817db882c3
Yai <myd.rio@hotmail.com>
2021-05-22 18:01:25 +0200

    dashboard nav pretty

--- client/src/components/_ui/DashboardNav/DashboardNav.jsx
+++ client/src/components/_ui/DashboardNav/DashboardNav.jsx
@@ -1,15 +1,14 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { types } from '../../../types/types'
 import { AuthContext } from '../../../reducers/auth/AuthContext';
 
-import { FaRegPlusSquare, FaSuitcase } from 'react-icons/fa';
-import { AiOutlineComment } from 'react-icons/ai';
-import { MdExitToApp, MdWeb } from 'react-icons/md';
+import { AiOutlineBank, AiOutlineCloseCircle, AiOutlineComment, AiOutlineLayout, AiOutlinePlusCircle } from 'react-icons/ai';
 
 export const DashboardNav = ({ handleShow }) => {
 	const { dispatch, user } = useContext(AuthContext)
+	const [currentNav, setCurrentNav] = useState()
 
 	const handleLogout = async () => {
 		await dispatch({ type: types.logout })
@@ -21,29 +20,28 @@ export const DashboardNav = ({ handleShow }) => {
 		<>
 			<nav className='dash-nav '>
 
+				<Link to='/bratic' onClick={ () => setCurrentNav('') }>
+					<figure>
+						<img src='https://res.cloudinary.com/bratic-app/image/upload/v1609866461/logoBN_ujpbti.svg' alt='' />
+					</figure>
+				</Link>
+
 				<ul>
 					<li>
-						<Link to='/bratic'>
-							<figure>
-								<img src='https://res.cloudinary.com/bratic-app/image/upload/v1609866461/logoBN_ujpbti.svg' alt='' />
-							</figure>
-						</Link>
+						<Link to='/bratic/empresa' onClick={ () => setCurrentNav('empresa') }><AiOutlineBank className={ currentNav === 'empresa' ? 'active' : null } /></Link>
 					</li>
 					<li>
-						<Link to='/bratic/empresa'><FaSuitcase /></Link>
+						<Link to='/bratic/blog' onClick={ () => setCurrentNav('blog') }><AiOutlineComment className={ currentNav === 'blog' ? 'active' : null } /></Link>
 					</li>
 					<li>
-						<Link to='/bratic/blog'><AiOutlineComment /></Link>
-					</li>
-					<li>
-						<Link to='/bratic/seccion'><MdWeb /></Link>
+						<Link to='/bratic/seccion' onClick={ () => setCurrentNav('secciones') }><AiOutlineLayout className={ currentNav === 'secciones' ? 'active' : null } /></Link>
 					</li>
 				</ul>
 				<div className='btn-group'>
 					{ user.email === 'admin@yai.com' && (
-						<FaRegPlusSquare onClick={ handleShow } />
+						<AiOutlinePlusCircle onClick={ handleShow } />
 					) }
-					<MdExitToApp onClick={ handleLogout } />
+					<AiOutlineCloseCircle onClick={ handleLogout } />
 				</div>
 			</nav>
 		</>
